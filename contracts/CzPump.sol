a449b989e357f0c0d55d8f53cd7e5bfee1e96298
PhiloCwh <105807963+PhiloCwh@users.noreply.github.com>
2025-03-07 03:05:17 +0800

    Update CzPump.sol

--- contracts/CzPump.sol
+++ contracts/CzPump.sol
@@ -9,13 +9,14 @@ import "./IERC20.sol";
 
 pragma solidity ^0.8.17;
 
-contract CzPump is Ownable{
+contract BSCBuilderPump is Ownable{
 
 
 //global variable
 
     Ifactory factory;
     IUniswapV2Router02 public uniswapRouter = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+    address payable immutable feeContract;
     
 
     mapping (address => address) public tokenCreator;
@@ -41,11 +42,12 @@ contract CzPump is Ownable{
     mapping (address => mapping (address => bool)) public isUserClaimed;
 
     //event Message
-    event DeployToken(address token,string name,string symbol, address creator,string description, string image, string website,string twLink,string tgLink,uint tokenAmount,uint time);
+    event DeployToken(address token,string name,string symbol, address creator,string description, string image, string website,string githubReposity,string twLink,uint idoAmount,uint userMaxIdoAmount,uint time);
 
-    constructor(address _factory)Ownable(msg.sender)
+    constructor(address _factory,address payable  _feeContract)Ownable(msg.sender)
     {
         factory = Ifactory(_factory);
+        feeContract = _feeContract;
     }
 
     receive() payable external {}
@@ -81,8 +83,9 @@ contract CzPump is Ownable{
 
     function idoComplited(address _token) public {
         require(tokenCreator[_token] == msg.sender,"not token creator");
-        require(tokenBnbBalance[_token] == tokenIdoBnbAmount[_token],"fuck");
+        require(tokenBnbBalance[_token] >= 99 * tokenIdoBnbAmount[_token] / 100,"fuck");
         IERC20(_token).approve(0x10ED43C718714eb63d5aA57B78B54704E256024E, 100000000 * ONE_ETH);
+        uint fee = tokenBnbBalance[_token] / 100;
         uniswapRouter.addLiquidityETH{value : tokenIdoBnbAmount[_token] / 2}(
             _token,                // ERC20 代币地址
             100000000 * ONE_ETH * 5 / 100,  // 希望添加的代币数量
@@ -94,12 +97,14 @@ contract CzPump is Ownable{
         tokenLaunched[_token] = true;
         tokenLastTimePrice[_token] = getPrice(_token);
         tokenClaimTime[_token] = block.timestamp;
-        payable (msg.sender).transfer(tokenIdoBnbAmount[_token] / 2);
+        payable (msg.sender).transfer((tokenBnbBalance[_token] - 100000000 * ONE_ETH * 5 / 100 - fee));
+        feeContract.transfer(fee);
         
     }
 
     function unlockToken(address _token) public {
-        require(block.timestamp - tokenClaimTime[_token] >= 180 * ONE_DAY,"require 180 days");
+        //require(block.timestamp - tokenClaimTime[_token] >= 180 * ONE_DAY,"require 180 days");
+        require(block.timestamp - tokenClaimTime[_token] >= 10 * ONE_DAY,"require 10 days");
         require(tokenCreator[_token] == msg.sender,"not creator");
         uint price = getPrice(_token);
         require(price >= 2 * tokenLastTimePrice[_token],"price not enought");
@@ -109,14 +114,14 @@ contract CzPump is Ownable{
     }
 
 
-    function launchIdo(string memory _name,string memory _symbol,string memory _image,string memory _description,string memory _website,string memory _twLink,string memory _tgLink,uint _idoBNBAmount,uint _userMaxIdoAmount) public reEntrancyMutex() returns(address){
+    function launchIdo(string memory _name,string memory _symbol,string memory _image,string memory _description,string memory _website,string memory _githubReposity,string memory _twLink,uint _idoBNBAmount,uint _userMaxIdoAmount) public reEntrancyMutex() returns(address){
         address _token = factory.createToken( msg.sender,_name, _symbol,_image);
 
         _beForeDeployToken(_token,msg.sender);
         tokenIdoBnbAmount[_token] = _idoBNBAmount;
         userMaxIdoAmount[_token] = _userMaxIdoAmount;
         
-        emit DeployToken(_token, _name, _symbol, msg.sender, _description, _image, _website, _twLink, _tgLink, 100000000 * ONE_ETH, block.timestamp);
+        emit DeployToken(_token, _name, _symbol, msg.sender, _description, _image, _website,_githubReposity, _twLink, _idoBNBAmount,_userMaxIdoAmount, block.timestamp);
         return _token;
         
     }
@@ -129,14 +134,14 @@ contract CzPump is Ownable{
     function renounceCreatorship(address _token) public {
         require(tokenCreator[_token] == msg.sender,"you are not creator");
         tokenCreator[_token] = address(0);
-        IERC20(_token).transfer(address(0),IERC20(_token).balanceOf(address(this)));
+        IERC20(_token).transfer(address(0),IERC20(_token).balanceOf(address(this)) - 100000000 * ONE_ETH * 5 / 100);
         
     }
-    
+
     function renounceCreatorshipAndSendToCZ(address _token) public {
         require(tokenCreator[_token] == msg.sender,"you are not creator");
         tokenCreator[_token] = address(0);
-        IERC20(_token).transfer(0x28816c4C4792467390C90e5B426F198570E29307,IERC20(_token).balanceOf(address(this)));
+        IERC20(_token).transfer(0x28816c4C4792467390C90e5B426F198570E29307,IERC20(_token).balanceOf(address(this)) - 100000000 * ONE_ETH * 5 / 100);
         
     }
 
