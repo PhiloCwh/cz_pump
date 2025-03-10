4ea01c4b8c6be7e2597ded259d6cbc155bdbdf0b
PhiloCwh <105807963+PhiloCwh@users.noreply.github.com>
2025-03-11 00:38:15 +0800

    Update TWAPV3.sol

--- contracts/TWAPV3.sol
+++ contracts/TWAPV3.sol
@@ -1,1 +1,18 @@
 
+contract TWAPOracleV3 {
+    address public pool;
+
+    constructor(address _pool) {
+        pool = _pool;
+    }
+
+    function getTwapPrice(uint32 interval) external view returns (int56 twapPrice) {
+        uint32;
+        secondsAgo[0] = interval;
+        secondsAgo[1] = 0;
+
+        (int56[] memory tickCumulatives,) = IUniswapV3Pool(pool).observe(secondsAgo);
+        int56 tickDifference = tickCumulatives[1] - tickCumulatives[0];
+        twapPrice = tickDifference / int56(uint56(interval));
+    }
+}
